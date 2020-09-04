115dd3c91416b3cc697720b266cb91e61f56a14f
amoq18 <dopegnonamoq18@gmail.com>
2020-09-04 14:35:20 +0200

    package crud + tremplating added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,265 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+    <!-- Page Header -->
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3 class="page-title">Welcome Admin!</h3>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /Page Header -->
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <div class="row">
+        <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="dash-widget-header">
+                        <span class="dash-widget-icon bg-primary">
+                            <i class="fe fe-users"></i>
+                        </span>
+                        <div class="dash-count">
+                            <i class="fa fa-arrow-up text-success"></i> 17%
+                        </div>
+                    </div>
+                    <div class="dash-widget-info">
+                        <h3>168</h3>
+                        <h6 class="text-muted">Customers</h6>
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-primary w-50"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="dash-widget-header">
+                        <span class="dash-widget-icon bg-success">
+                            <i class="fe fe-money"></i>
+                        </span>
+                        <div class="dash-count">
+                            <i class="fa fa-arrow-down text-danger"></i> 12%
+                        </div>
+                    </div>
+                    <div class="dash-widget-info">
+                        <h3>21587</h3>
+                        <h6 class="text-muted">Products</h6>
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-success w-50"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="dash-widget-header">
+                        <span class="dash-widget-icon bg-danger">
+                            <i class="fe fe-credit-card"></i>
+                        </span>
+                        <div class="dash-count">
+                            <i class="fa fa-arrow-down text-danger"></i> 12%
+                        </div>
+                    </div>
+                    <div class="dash-widget-info">
+                        <h3>$56485</h3>
+                        <h6 class="text-muted">Sales</h6>
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-danger w-50"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="dash-widget-header">
+                        <span class="dash-widget-icon bg-warning">
+                            <i class="fe fe-folder"></i>
+                        </span>
+                        <div class="dash-count">
+                            <i class="fa fa-arrow-up text-success"></i> 17%
+                        </div>
+                    </div>
+                    <div class="dash-widget-info">
+                        <h3>$62523</h3>
+                        <h6 class="text-muted">Revenue</h6>
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-warning w-50"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-lg-6">
+
+            <!-- Sales Chart -->
+            <div class="card card-chart">
+                <div class="card-header">
+                    <h4 class="card-title">Sales Overview</h4>
+                </div>
+                <div class="card-body">
+                    <div id="morrisArea"></div>
+                </div>
+            </div>
+            <!-- /Sales Chart -->
+
+        </div>
+        <div class="col-md-12 col-lg-6">
+
+            <!-- Invoice Chart -->
+            <div class="card card-chart">
+                <div class="card-header">
+                    <h4 class="card-title">Order Status</h4>
+                </div>
+                <div class="card-body">
+                    <div id="morrisLine"></div>
+                </div>
+            </div>
+            <!-- /Invoice Chart -->
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 d-flex">
+
+            <!-- Recent Orders -->
+            <div class="card card-table flex-fill">
+                <div class="card-header">
+                    <h4 class="card-title">Recent Orders</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-center">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Date</th>
+                                    <th class="text-center">Quantity</th>
+                                    <th class="text-center">Status</th>
+                                    <th class="text-right">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-nowrap">
+                                        <div class="font-weight-600">Apple Watch Series 4</div>
+                                    </td>
+                                    <td class="text-nowrap">19 Jan 2019</td>
+                                    <td class="text-center">5</td>
+                                    <td class="text-center">
+                                        <span class="badge badge-pill bg-success inv-badge">Completed</span>
+                                    </td>
+                                    <td class="text-right">
+                                        <div class="font-weight-600">$487</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-nowrap">
+                                        <div class="font-weight-600">Apple iPhone XR</div>
+                                    </td>
+                                    <td class="text-nowrap">20 Jan 2019</td>
+                                    <td class="text-center">2</td>
+                                    <td class="text-center">
+                                        <span class="badge badge-pill bg-success inv-badge">Completed</span>
+                                    </td>
+                                    <td class="text-right">
+                                        <div class="font-weight-600">$255</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-nowrap">
+                                        <div class="font-weight-600">Dell XPS 9370</div>
+                                    </td>
+                                    <td class="text-nowrap">21 Jan 2019</td>
+                                    <td class="text-center">1</td>
+                                    <td class="text-center">
+                                        <span class="badge badge-pill bg-warning inv-badge">Pending</span>
+                                    </td>
+                                    <td class="text-right">
+                                        <div class="font-weight-600">$799</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-nowrap">
+                                        <div class="font-weight-600">Cisco WS-C2960X-48FPS-L</div>
+                                    </td>
+                                    <td class="text-nowrap">22 Jan 2019</td>
+                                    <td class="text-center">1</td>
+                                    <td class="text-center">
+                                        <span class="badge badge-pill bg-success inv-badge">Completed</span>
+                                    </td>
+                                    <td class="text-right">
+                                        <div class="font-weight-600">$970</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-nowrap">
+                                        <div class="font-weight-600">Apple MacBook Pro</div>
+                                    </td>
+                                    <td class="text-nowrap">23 Jan 2019</td>
+                                    <td class="text-center">1</td>
+                                    <td class="text-center">
+                                        <span class="badge badge-pill bg-danger inv-badge">Cancel</span>
+                                    </td>
+                                    <td class="text-right">
+                                        <div class="font-weight-600">$400</div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- /Recent Orders -->
+
+        </div>
+        <div class="col-md-6 d-flex">
+
+            <!-- Feed Activity -->
+            <div class="card flex-fill">
+                <div class="card-header">
+                    <h4 class="card-title">Feed Activity</h4>
+                </div>
+                <div class="card-body">
+                    <ul class="activity-feed">
+                        <li class="feed-item">
+                            <div class="feed-date">Apr 13</div>
+                            <span class="feed-text"><a href="profile.html">John Doe</a> added new product <a href="product-details.html">"Smart Watch"</a></span>
+                        </li>
+                        <li class="feed-item">
+                            <div class="feed-date">Mar 21</div>
+                            <span class="feed-text"><a href="profile.html">Justin Lee</a> requested amount of <a href="invoice.html">$5,781</a></span>
+                        </li>
+                        <li class="feed-item">
+                            <div class="feed-date">Feb 2</div>
+                            <span class="feed-text">New user registered <a href="profile.html">"Mary Wiley"</a></span>
+                        </li>
+                        <li class="feed-item">
+                            <div class="feed-date">Jan 27</div>
+                            <span class="feed-text"><a href="profile.html">Robert Martin</a> gave a review for <a href="product-details.html">"Dell Laptop"</a></span>
+                        </li>
+                        <li class="feed-item">
+                            <div class="feed-date">Jan 14</div>
+                            <span class="feed-text">New customer registered <a href="profile.html">"Tori Carter"</a></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /Feed Activity -->
+
+        </div>
+    </div>
+@endsection
